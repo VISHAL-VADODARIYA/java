3540b95684daa3e5e0068076c20ad14b7e6e3276
VISHAL-VADODARIYA <vadodariyavishal2@gmail.com>
2023-10-17 15:05:13 +0530

    security project added

--- spring-security-tutorial/spring-security-client/src/main/java/com/vishal/springsecurityclient/SpringSecurityClientApplication.java
+++ spring-security-tutorial/spring-security-client/src/main/java/com/vishal/springsecurityclient/SpringSecurityClientApplication.java
@@ -1,4 +1,4 @@
-package com.learning.springsecurityclient;
+package com.vishal.springsecurityclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-security-tutorial/spring-security-client/src/test/java/com/vishal/springsecurityclient/SpringSecurityClientApplicationTests.java
+++ spring-security-tutorial/spring-security-client/src/test/java/com/vishal/springsecurityclient/SpringSecurityClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learning.springsecurityclient;
+package com.vishal.springsecurityclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
